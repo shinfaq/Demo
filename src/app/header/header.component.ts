9828e9e8f92578cb16b752a277f6ca22baa41a0e
shinfaq <ngocwtf@gmail.com>
2020-09-01 09:54:52 +0700

    add login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  user = "";
+  isLogin = false;
+  constructor(
+    private cookieService: CookieService
+  ) { }
 
   ngOnInit(): void {
+    if(this.cookieService.get("id")){
+     this.user = this.cookieService.get("id");
+     this.isLogin = true;
+    }
   }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,10 +2,11 @@ import { AlertService } from './../services/alert.service';
 import { UserService } from './../services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   listUsers;
@@ -14,34 +15,32 @@ export class LoginComponent implements OnInit {
   constructor(
     private userService: UserService,
     private alertService: AlertService,
-    private router: Router
-  ) {
-
-  }
+    private router: Router,
+    private cookieService: CookieService
+  ) {}
 
   ngOnInit(): void {
-    this.userService.getAllUser().subscribe(res => {
-      this.listUsers = res
-    })
+    this.userService.getAllUser().subscribe((res) => {
+      this.listUsers = res;
+    });
   }
   login() {
     let k = false;
-    this.listUsers.forEach(element => {
+    this.listUsers.forEach((element) => {
       if (this.user == element.name) {
         if (this.pass == element.password) {
-          this.alertService.showSuccessAlert("Login success");
+          this.alertService.showSuccessAlert('Login success');
           k = true;
+          this.cookieService.set('id', this.user);
           setTimeout(() => {
-            this.router.navigateByUrl("/home")
+            this.router.navigateByUrl('/home');
           }, 3000);
-        }
-        else {
-          this.alertService.showErrorAlert("Your password not true!");
+        } else {
+          this.alertService.showErrorAlert('Your password not true!');
           k = true;
         }
       }
     });
-    if (k == false)
-      this.alertService.showErrorAlert("User does not exist!")
+    if (k == false) this.alertService.showErrorAlert('User does not exist!');
   }
 }
